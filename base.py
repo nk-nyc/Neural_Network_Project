450ff9a9f4bc667c101bc9d4b4b2ae2613605bc8
Natalie <nataliekkeiger@gmail.com>
2024-06-09 15:40:03 -0400

    Final update

--- base.py
+++ base.py
@@ -1,12 +1,14 @@
-
 import random
 rand=random.Random()
 from math import exp
 
+global LEARNING_RATE
+LEARNING_RATE = 0.2
+
 # training = True/False whether this is training vs. test data
 # n = number of images/labels to get
 # returns array of (pixels, label)
-def get_data(training=True, n=100):
+def get_data(training=True, n=5000):
   lbl_f = open(f"data/train-labels-idx1-ubyte", "rb")   # MNIST has labels (digits)
   img_f = open(f"data/train-images-idx3-ubyte", "rb")     # and pixel vals separate
 
@@ -33,20 +35,16 @@ def test_print(item):
         print("X", end='')
     print("")
 
-
-print("Loading data...")
-data = get_data()
-print(f"Loaded {len(data)} items")
-
-# Test one of them
-test_print(data[0])
-
 def sigmoid(x):
     if x < -500:
        return 0
     elif x > 500:
-       return 1 
+       return 1
     return 1/(1+exp(-x))
+
+#returns the derivative of the sigmoid function
+def sig_deriv(x):
+   return x * (1.0 - x)
 
 # Four layers:
 # 1. input layer
@@ -57,16 +55,17 @@ def sigmoid(x):
 class Neuron:
     def __init__(self, random_weights=None):
         # The actual number held in this neuron
-        self.value = 0 
+        self.value = 0
+        self.delta = 0
         self.weights = []
-        self.bias = round(rand.uniform(-1, 1), 4)
+        self.bias = rand.uniform(-1, 1)
         if random_weights:
             for _ in range(random_weights):
-                self.weights.append(round (rand.uniform(-1, 1), 4)) #assigns arbitrary weights between 0 and 2
+                self.weights.append(rand.uniform(-1, 1)) #assigns arbitrary weights between 0 and 2
    
     def __str__(self):
        return f"Neuron:{self.weights}"
-    
+   
     # Inputs is a list of Neurons that correspond to the weights
     def forward_propagate(self, inputs):
       self.value = self.bias
@@ -79,7 +78,6 @@ class Layer:
         self.neurons = []
         for _ in range(neurons):
             self.neurons.append(Neuron(random_weights))
-        
    
     def __str__(self):
        return f"Layer:{[str(n) for n in self.neurons]}"
@@ -90,20 +88,9 @@ class Layer:
        for neuron in self.neurons:
           neuron.forward_propagate(inputs)
 
-input_layer = Layer(neurons=784,random_weights=0)
-# layer1 = Layer(neurons=16,random_weights=784)
-output_layer = Layer(neurons=10, random_weights=16)
-layer2 = Layer(neurons=16, random_weights=784)
-
-network = [
-  input_layer,
-  layer2,
-  output_layer
-]
-
 #need to write backpropagation that can plug into layer and neuron values
 #cost function
-def execute_nn(image_pixels):    
+def execute_nn(image_pixels, network):    
     for i in range(len(network)):
       if i == 0: # input layer
         for j in range(len(image_pixels)):
@@ -114,14 +101,87 @@ def execute_nn(image_pixels):
 def mean_squared_error(actual_values, predicted_values):
    return 1/(len(actual_values)) * (sum([(actual_values[n] - predicted_values[n])**2 for n in range(len(actual_values))]))
 
-def cost(values, labels):
-   mean_squared_error(values, )
+#stochastic gradient descent:
+#randomly initialize values
+#set parameters (number of iterations and learning rate)
+#SGD loop:
+    #Shuffle the training dataset for randomness
+    #Iterate over each training example in that order
+    #Compute the gradient of the cost function with respect to the model parameters using the current training example
+    #Update the model parameters in the direction of the negative gradient, scaled by the LR
+    #Evaluate the difference in the cost function between iterations
+#When max. iterations are complete or the convergence criteria are met, return those parameters
+#https://www.geeksforgeeks.org/ml-stochastic-gradient-descent-sgd/
+#https://realpython.com/gradient-descent-algorithm-python/
+       
+def gradient_descent(gradient, start, num_epochs, lr, tol):
+        vector = start
+        for _ in range(num_epochs):
+            diff = -lr * gradient(vector)
+            if abs(diff) <= tol:
+                break
+            vector += diff
+        return vector
+#start should be able to take an array
 
-def execute_for_image(image_index):
-  (pixels, label) = data[image_index]
-  execute_nn(pixels)
-  for i in range(10):
-    print(f"{i}:{output_layer.neurons[i].value}")
-  return(mean_squared_error([n.value for n in output_layer.neurons], [0 if n != label else 1 for n in range(10)]))
+#error = (output - expected) * sig_deriv(output) in output layer
 
-execute_for_image(0)
+def backward_propagate():
+  pass
+
+def main():
+
+# print("Loading data...")
+  data = get_data()
+# print(f"Loaded {len(data)} items")
+
+# execute_for_image(0)
+   
+  input_layer = Layer(neurons=784,random_weights=0)
+  layer1 = Layer(neurons=16,random_weights=784)
+  layer2 = Layer(neurons=16, random_weights=16)
+  output_layer = Layer(neurons=10, random_weights=16)
+
+  network = [
+    input_layer,
+    layer1,
+    layer2,
+    output_layer
+  ]
+  
+  for epoch in range(5001):
+    for image_index in range(len(data)): # eventually len(data)
+      (orig_pixels, label) = data[image_index]
+      pixels = [n/256.0 for n in orig_pixels]
+      execute_nn(pixels, network)
+      for neuron_index, neuron in enumerate(output_layer.neurons):
+        neuron.delta = (neuron.value - (0 if neuron_index != label else 1)) * sig_deriv(neuron.value)
+      for layer_index in reversed(range(len(network))):
+        if layer_index == 0 or layer_index == len(network) - 1:
+           continue
+        layer = network[layer_index]
+        for neuron_index, neuron in enumerate(layer.neurons):
+          neuron.delta = 0
+          for i in range(len(network[layer_index + 1].neurons)):
+            deriv = sig_deriv(neuron.value)
+            future_error = network[layer_index+1].neurons[i].delta
+            my_weight = network[layer_index + 1].neurons[i].weights[neuron_index]
+            neuron.delta += deriv * future_error * my_weight
+          
+      for layer in network[1:-1]: 
+        for neuron in layer.neurons:
+            for i in range(len(neuron.weights)):
+              neuron.weights[i] = neuron.weights[i] - LEARNING_RATE * neuron.delta * neuron.value
+            neuron.bias = neuron.bias - LEARNING_RATE * neuron.delta
+        
+    if epoch % 1 == 0:
+      total_mse = 0
+      for image_index in range(len(data)):
+        (orig_pixels, label) = data[image_index]
+        pixels = [n/256.0 for n in orig_pixels]
+        execute_nn(pixels, network)
+        total_mse += mean_squared_error([n.value for n in network[-1].neurons], [0 if n != label else 1 for n in range(10)])
+      print(epoch, total_mse)
+
+if __name__ == "__main__":
+    main()
